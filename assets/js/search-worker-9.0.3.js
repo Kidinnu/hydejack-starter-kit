ec7305a5795f0aa2b177d17926a9d031485197fa
Kidinnu <kidinnu@hotmail.com>
2020-09-09 23:10:00 +0400

    init2

--- assets/js/search-worker-9.0.3.js
+++ assets/js/search-worker-9.0.3.js
@@ -47,7 +47,7 @@ async function getDocuments() {
 ///////////////////////////////////////////////////////////////////////////////
 
 importScripts('/assets/js/minisearch/dist/umd/index.min.js');
-const INDEX_KEY = 'index--2020-09-09T22:48:10+04:00';
+const INDEX_KEY = 'index--2020-09-09T23:08:46+04:00';
 const storage = new StorageArea('mini-search/');
 
 const OPTIONS = {
