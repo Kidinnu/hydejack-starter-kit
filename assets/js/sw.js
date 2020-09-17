3b049fdb3aa73b48007a683d35a0a0259f95f9d9
Kidinnu <kidinnu@hotmail.com>
2020-09-17 20:28:31 +0400

    task 1 mbs

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -29,7 +29,7 @@ const SHELL_CACHE = "shell-9.0.3--v1--sw/";
 const ASSETS_CACHE = "assets--v1--sw/";
 
 // The cache for regular content, which will be invalidated every time you make a new build.
-const CONTENT_CACHE = "content--2020-09-12T22:07:02+04:00--sw/";
+const CONTENT_CACHE = "content--2020-09-17T20:28:06+04:00--sw/";
 
 // A URL search parameter you can add to external assets to cache them in the service worker.
 const SW_CACHE_SEARCH_PARAM = "sw-cache";
@@ -117,6 +117,7 @@ const STATIC_FILES = [
   /**/"/assets/img/rocket-ship.png",
   /**/"/assets/img/rocket.png",
   /**/"/assets/img/satellite.png",
+  /**/"/assets/img/satmatrix.jpg",
   /**/"/assets/img/scheme_pull.png",
   /**/"/assets/img/scheme_push.png",
   /**/"/assets/img/script_current.png",
