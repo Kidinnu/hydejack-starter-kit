6b25a2e989b5e6f4cc35d5cdd656a41cede6ae01
Kidinnu <kidinnu@hotmail.com>
2020-10-02 21:57:31 +0400

    matlab test 1

--- assets/js/search-worker-9.0.3.js
+++ assets/js/search-worker-9.0.3.js
@@ -47,7 +47,7 @@ async function getDocuments() {
 ///////////////////////////////////////////////////////////////////////////////
 
 importScripts('/assets/js/minisearch/dist/umd/index.min.js');
-const INDEX_KEY = 'index--2020-09-20T14:24:34+04:00';
+const INDEX_KEY = 'index--2020-10-02T21:56:52+04:00';
 const storage = new StorageArea('mini-search/');
 
 const OPTIONS = {
