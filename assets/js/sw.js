66dfb5bbc6f96fc84af684d780da32bedc8c74ae
Kidinnu <kidinnu@hotmail.com>
2020-10-18 18:19:07 +0400

    matlab lab3

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -29,7 +29,7 @@ const SHELL_CACHE = "shell-9.0.3--v1--sw/";
 const ASSETS_CACHE = "assets--v1--sw/";
 
 // The cache for regular content, which will be invalidated every time you make a new build.
-const CONTENT_CACHE = "content--2020-10-17T21:52:06+04:00--sw/";
+const CONTENT_CACHE = "content--2020-10-18T18:18:40+04:00--sw/";
 
 // A URL search parameter you can add to external assets to cache them in the service worker.
 const SW_CACHE_SEARCH_PARAM = "sw-cache";
