94f76c164df0b3a0f15a9d327e8344eb0cd5da97
Kidinnu <kidinnu@hotmail.com>
2020-11-04 11:04:55 +0400

    lab4

--- assets/js/search-worker-9.0.3.js
+++ assets/js/search-worker-9.0.3.js
@@ -47,7 +47,7 @@ async function getDocuments() {
 ///////////////////////////////////////////////////////////////////////////////
 
 importScripts('/assets/js/minisearch/dist/umd/index.min.js');
-const INDEX_KEY = 'index--2020-11-04T09:21:55+04:00';
+const INDEX_KEY = 'index--2020-11-04T11:04:33+04:00';
 const storage = new StorageArea('mini-search/');
 
 const OPTIONS = {
