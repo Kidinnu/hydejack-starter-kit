06e675a4936c694d3704122b55c73307e429a764
Vadim <kidinnu@hotmail.com>
2020-11-20 14:31:43 +0400

    test

--- assets/js/search-worker-9.0.3.js
+++ assets/js/search-worker-9.0.3.js
@@ -47,7 +47,7 @@ async function getDocuments() {
 ///////////////////////////////////////////////////////////////////////////////
 
 importScripts('/assets/js/minisearch/dist/umd/index.min.js');
-const INDEX_KEY = 'index--2020-11-20T14:03:17+04:00';
+const INDEX_KEY = 'index--2020-11-20T14:31:21+04:00';
 const storage = new StorageArea('mini-search/');
 
 const OPTIONS = {
