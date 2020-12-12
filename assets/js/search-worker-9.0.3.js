ed26bc51a741afc314c2192e843e151b7d578a73
Vadim <kidinnu@hotmail.com>
2020-12-12 16:19:18 +0400

    lagrange

--- assets/js/search-worker-9.0.3.js
+++ assets/js/search-worker-9.0.3.js
@@ -47,7 +47,7 @@ async function getDocuments() {
 ///////////////////////////////////////////////////////////////////////////////
 
 importScripts('/assets/js/minisearch/dist/umd/index.min.js');
-const INDEX_KEY = 'index--2020-12-11T19:13:38+04:00';
+const INDEX_KEY = 'index--2020-12-12T16:18:50+04:00';
 const storage = new StorageArea('mini-search/');
 
 const OPTIONS = {
