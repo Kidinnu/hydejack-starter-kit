98102c7860da7b15f69f467f089c296f5634a752
kidinnu <kidinnu@hotmail.com>
2021-01-30 19:38:34 +0400

    test

--- assets/js/search-worker-9.0.3.js
+++ assets/js/search-worker-9.0.3.js
@@ -47,7 +47,7 @@ async function getDocuments() {
 ///////////////////////////////////////////////////////////////////////////////
 
 importScripts('/assets/js/minisearch/dist/umd/index.min.js');
-const INDEX_KEY = 'index--2021-01-30T19:36:23+04:00';
+const INDEX_KEY = 'index--2021-01-30T19:38:21+04:00';
 const storage = new StorageArea('mini-search/');
 
 const OPTIONS = {
